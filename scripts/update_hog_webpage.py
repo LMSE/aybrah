e799a0758bb3395205e28c02866cd59d4754fb9b
kcorreia <kevin.correia@gmail.com>
2018-10-03 01:28:32 -0700

    update curation column

--- scripts/update_hog_webpage.py
+++ scripts/update_hog_webpage.py
@@ -6,7 +6,7 @@ from os.path import isfile, join
 path_webiste='./website/'
 
 # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
-onlyfiles = [f for f in listdir(mypath) if isfile(join(path_webiste, f)) and 'phobius' not in f ]
+onlyfiles = [f for f in listdir(path_webiste) if isfile(join(path_webiste, f)) and 'phobius' not in f ]
 
 
 header='<a href="../index.html">Home</a><br><br>'
@@ -19,3 +19,24 @@ for file_html in onlyfiles:
 	page=page.replace('#banner { background: #bdbdbd; height:200px;','#banner { background: #bdbdbd; height:225px;')
 	page=page.replace(header,header+search_box)
 	open(path_webiste+file_html,'w').write(page)
+
+
+############################################################
+
+
+for file_html in onlyfiles:
+	hog=file_html.split('.')[0]
+	print(hog)
+	# get link for GitHub suggestion
+	url_aybrah='https://kcorreia.github.io/aybrah/website/'+hog+'.html'
+	url_github='https://github.com/kcorreia/aybrah/issues/new?body=%23%20Description%20of%20the%20issue%0A%0A%0A%0A%23%20Page%0A'+url_aybrah
+	page=open(path_webiste+file_html,'r').read()
+	page=page.replace('<head>','<head><title>'+hog+'</title>')
+	page=page.replace('#banner { background: #bdbdbd; height:225px;','#banner { background: #bdbdbd; height:265px;')
+	page=page.replace('Phobius predictions</a></div>','Phobius predictions</a><br><br>\n'+'\t'*17+'<a href="'+url_github+'">Report suggestion on GitHub</a></div>')
+	open(path_webiste+file_html,'w').write(page)
+
+
+
+
+
